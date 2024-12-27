33402ce26679f2fdcd3b86a29fdb2219ab5fb888
Saki Sekido <ponzu.ss0039@gmail.com>
2024-12-27 14:06:40 +0900

    プロダクトコレクション前

--- original/CameraViewController.swift
+++ original/CameraViewController.swift
@@ -9,15 +9,78 @@ import UIKit
 
 class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
+    var number: Int!
     
     @IBOutlet var photoImageView: UIImageView!
     @IBOutlet var cameraButton: UIButton!
+    @IBOutlet var missionLabel: UILabel!
+    @IBOutlet var missionLabel2: UILabel!
+    @IBOutlet var missionLabel3: UILabel!
+    @IBOutlet var missionLabel4: UILabel!
+    @IBOutlet var saveButton: UIButton!
+    
+    var showMission: String!
+    var missions: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        missionLabel2.isHidden = false
+        missionLabel3.isHidden = false
+        missionLabel.isHidden = true
+        missionLabel4.isHidden = true
+        saveButton.isHidden = true
+        
+        missions =
+        ["今日の服を写そう"]
+        
+//        missions = ["水や鏡などに映った面白い反射を撮ってみよう","写真の中に自分の指を隠して撮影してみよう","わざと写真を逆さまにして撮ってみよう","しましま模様があるものを見つけよう", "日本的なものを撮影してみよう", "未来を感じさせる何かを撮影しよう", "左右対称の構図で撮影してみよう", "何かのマークやロゴを見つけて撮影しよう", "今日の服を写そう", "英語が書かれているものを見つけて撮影", "ミッションが見つかりませんでした" ]
+        number = Int.random(in: 0..<missions.count)
+        showMission = missions[number]
+        
+        
+        missionLabel.text = showMission
+        missionLabel2.text = showMission
+        
+        
+//        switch number {
+//        case 0:
+//                    missionLabel.text = "水や鏡などに映った面白い反射を撮ってみよう"
+//            showMision =
+//                case 1:
+//                    missionLabel.text = "写真の中に自分の指を隠して撮影してみよう"
+//                case 2:
+//                    missionLabel.text = "わざと写真を逆さまにして撮ってみよう"
+//                case 3:
+//                    missionLabel.text = "しましま模様があるものを見つけよう"
+//                case 4:
+//                    missionLabel.text = "日本的なものを撮影してみよう"
+//                case 5:
+//                    missionLabel.text = "未来を感じさせる何かを撮影しよう"
+//                case 6:
+//                    missionLabel.text = "左右対称の構図で撮影してみよう"
+//                case 7:
+//                    missionLabel.text = "何かのマークやロゴを見つけて撮影しよう"
+//                case 8:
+//                    missionLabel.text = "今日の服を写そう"
+//                case 9:
+//                    missionLabel.text = "英語が書かれているものを見つけて撮影"
+//                default:
+//                    missionLabel.text = "ミッションが見つかりませんでした"
+//                }
+        
         cameraButton.layer.borderColor = UIColor.black.cgColor  // 枠線の色
         cameraButton.layer.borderWidth = 1.0 // 枠線の太さ
         cameraButton.layer.cornerRadius = 10 //ボタンの角を丸める
+        
+        missionLabel.font = UIFont(name: "DelaGothicOne-Regular", size: 30)
+        missionLabel.adjustsFontSizeToFitWidth = true
+        missionLabel2.font = UIFont(name: "DelaGothicOne-Regular", size: 30)
+        missionLabel2.adjustsFontSizeToFitWidth = true
+        missionLabel3.font = UIFont(name: "DelaGothicOne-Regular", size: 18)
+        missionLabel4.font = UIFont(name: "DelaGothicOne-Regular", size: 18)
+//        saveButton.titleLabel?.font = UIFont(name: "DelaGothicOne-Regular", size: 20)
+//        saveButton.font = UIFont(name: "DelaGothicOne-Regular", size: 20)
     }
     
     @IBAction func onTappedCameraButton() {
@@ -42,29 +105,43 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         self.dismiss(animated: true, completion: nil)
         
-        let image = info[.originalImage] as! UIImage
-        let size = CGSize(width: image.size.width / 8, height: image.size.height / 8)
-        let resizedImage = UIGraphicsImageRenderer(size: size).image { _ in image.draw(in: CGRect(origin: .zero, size: size))
-            photoImageView.image = image
-            
+        guard let originalImage = info[.originalImage] as? UIImage else {
+            return
         }
+        
+        let imageWithText = drawText(image: originalImage)
+        
+        photoImageView.image = imageWithText
+//
+//        let image = info[.originalImage] as! UIImage
+//        let size = CGSize(width: image.size.width / 8, height: image.size.height / 8)
+//        let resizedImage = UIGraphicsImageRenderer(size: size).image { _ in image.draw(in: CGRect(origin: .zero, size: size))
+       
+//            
+        missionLabel2.isHidden = true
+        missionLabel3.isHidden = true
+        cameraButton.isHidden = true
+        missionLabel.isHidden = false
+        missionLabel4.isHidden = false
+        saveButton.isHidden = false
+        
     }
     
     
     func drawText(image: UIImage) -> UIImage{
         
-        let text = "post1"
+        let text = showMission!
         
         let fontSize = image.size.width / 10
-        let textFontAttributes = [
-            NSAttributedString.Key.font: UIFont(name: "Dela Gothic One", size: fontSize)!,
-            NSAttributedString.Key.foregroundColor: UIColor.white
+        let textFontAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont(name: "DelaGothicOne-Regular", size: fontSize)!,
+            .foregroundColor: UIColor.white
 
         ]
         
         // テキストのサイズを計算
             let textSize = text.size(withAttributes: textFontAttributes)
-            
+//            
             // 中央揃えのX座標
             let textX = (image.size.width - textSize.width) / 2
             
@@ -112,14 +189,14 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
 //        
 //        return newImage!
 //    }
-    
-    @IBAction func onTappedTextButton() {
-        if photoImageView.image != nil {
-            photoImageView.image = drawText(image: photoImageView.image!)
-        } else {
-            print("画像がありません")
-        }
-    }
+//    
+//    @IBAction func onTappedTextButton() {
+//        if photoImageView.image != nil {
+//            photoImageView.image = drawText(image: photoImageView.image!)
+//        } else {
+//            print("画像がありません")
+//        }
+//    }
     
 //    @IBAction func onTappedIllustButton() {
 //        if photoImageView.image != nil {
@@ -128,6 +205,8 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
 //            print("画像がありません")
 //        }
 //    }
+    
+    
     
     //保存
     @IBAction func onTappedUploadButton() {
@@ -140,4 +219,7 @@ class CameraViewController: UIViewController, UINavigationControllerDelegate, UI
             print("画像がありません")
         }
     }
+    
+    
+
 }
