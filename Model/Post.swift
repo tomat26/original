054384e0577ec384e4b5c0eddc5a058c4aaa16be
Saki Sekido <ponzu.ss0039@gmail.com>
2025-01-25 22:38:59 +1300

    postが持つデータを追加

--- Model/Post.swift
+++ Model/Post.swift
@@ -21,15 +21,19 @@ struct Post: Codable {
     let profileImage: String
     let username: String
     let postImage: String
+    let comments: String
+    let goodButton: String
+    let userid: String
+    let createdAt: String
 }
 
 // MARK: - ダミーデータ
 struct DummyData {
     static let posts: [Post] = [
-        Post(id: "1", profileImage: "profile1", username: "Alice", postImage: "post1"),
-        Post(id: "2", profileImage: "profile2", username: "Bob", postImage: "post2"),
-        Post(id: "3", profileImage: "profile3", username: "Charlie", postImage: "post3"),
-        Post(id: "4", profileImage: "profile4", username: "Diana", postImage: "post4")
+        Post(id: "1", profileImage: "profile1", username: "Alice", postImage: "post1",comments: "", goodButton: "", userid: "", createdAt: ""),
+        Post(id: "2", profileImage: "profile2", username: "Bob", postImage: "post2", comments: "", goodButton: "", userid: "", createdAt: ""),
+        Post(id: "3", profileImage: "profile3", username: "Charlie", postImage: "post3", comments: "", goodButton: "", userid: "", createdAt: ""),
+        Post(id: "4", profileImage: "profile4", username: "Diana", postImage: "post4", comments: "", goodButton: "", userid: "", createdAt: "")
     ]
 }
 
